6338778c58eab8d31a93685fc0a13a4a995f1bdb
Dzii <zikrigumilar2008@gmail.com>
2024-11-21 08:30:48 +0700

    Menambahkan semua file dan media ke repository

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    
+    navLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
+
